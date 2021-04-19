9c1f1928fc299c40001d07e1b3bcb27efb5c292e
steveallison1980 <stephen.allison@patest.co.jp>
2021-04-19 13:08:57 +0900

    added language toggle functions for professionals page

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
+// import { AngularFirestore } from '@angular/fire/firestore'; AAA
 import { Observable } from 'rxjs';
 
 @Component({
@@ -12,5 +12,5 @@ export class AppComponent {
   items: Observable<any[]>;
   constructor(firestore: AngularFirestore) {
      //this.items = firestore.collection('items').valueChanges();
-  }
+  } 
 }
