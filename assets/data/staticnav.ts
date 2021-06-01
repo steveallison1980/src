106264a9a195e18acc25247494a9bdb31cd77daf
steveallison1980 <stephen.allison@patest.co.jp>
2021-06-01 13:36:56 +0900

    practice areas links bug fix

--- assets/data/staticnav.ts
+++ assets/data/staticnav.ts
@@ -66,6 +66,21 @@ export const SITEMAP_DATA: ISitemap =
             displayMode: ""
         },
         {
+            mainpage: 'careers',
+            routerlink: '/careerspage',
+            text: "Careers",
+            textJP: "採用情報",
+            defaultsubpage: 'careerspage',
+            links: [
+                { text: 'Careers', textJP: 'キャリア', icon: 'person_add', subPage: 'careerspage', curPage: 'careers', routerlink: '/careerspage', displayMode: 'ENonly' },
+                { text: '', textJP: '事務所説明会', icon: 'accessibility_new', subPage: 'setsumeikaipage', curPage: 'careers', routerlink: '/setsumeikaipage', displayMode: 'JPonly' },
+                { text: '', textJP: 'ご案内', icon: 'line_style', subPage: 'setsumeikaiguidepage', curPage: 'careers', routerlink: '/setsumeikaiguidepage', displayMode: 'JPonly' },
+                { text: '', textJP: '募集要項', icon: 'work_outline', subPage: 'boshuyokopage', curPage: 'careers', routerlink: '/boshuyokopage', displayMode: 'JPonly' },
+                { text: '', textJP: '仕事環境', icon: 'supervised_user_circle', subPage: 'workenvironmentpage', curPage: 'careers', routerlink: '/workenvironmentpage', displayMode: 'JPonly' },
+            ],
+            displayMode: ""
+        },
+        {
             mainpage: 'cafcreports',
             routerlink: '/cafcall',
             text: "",
@@ -90,20 +105,6 @@ export const SITEMAP_DATA: ISitemap =
                 { text: '', textJP: '2005年', icon: 'menu_book', subPage: 'cafc2005', curPage: 'cafcreports', routerlink: '/cafc2005', displayMode: 'both' },
             ], 
             displayMode: "JPonly",
-        },
-        {
-            mainpage: 'careers',
-            routerlink: '/careerspage',
-            text: "Careers",
-            textJP: "採用情報",
-            defaultsubpage: 'careerspage',
-            links: [
-                { text: 'Careers', textJP: 'キャリア', icon: 'person_add', subPage: 'careerspage', curPage: 'careers', routerlink: '/careerspage', displayMode: 'ENonly' },
-                { text: '', textJP: '事務所説明会', icon: 'accessibility_new', subPage: 'setsumeikaipage', curPage: 'careers', routerlink: '/setsumeikaipage', displayMode: 'JPonly' },
-                { text: '', textJP: 'ご案内', icon: 'line_style', subPage: 'setsumeikaiguidepage', curPage: 'careers', routerlink: '/setsumeikaiguidepage', displayMode: 'JPonly' },
-                { text: '', textJP: '募集要項', icon: 'work_outline', subPage: 'boshuyokopage', curPage: 'careers', routerlink: '/boshuyokopage', displayMode: 'JPonly' },
-                { text: '', textJP: '仕事環境', icon: 'supervised_user_circle', subPage: 'workenvironmentpage', curPage: 'careers', routerlink: '/workenvironmentpage', displayMode: 'JPonly' },
-            ],
-            displayMode: ""
-        }]
+        }
+    ]
 };
